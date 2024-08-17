5a16dffa1b36863254b89f0711fc4beeb2b170f6
TheBaconStyle <maks.christmas2001@gmail.com>
2024-08-17 16:18:31 +0200

    feat(db): add data tables for internal functions

--- db/schema.ts
+++ db/schema.ts
@@ -12,3 +12,41 @@ export const users = schema.table("user", {
   verificationToken: text("verificationToken"),
   resetPasswordToken: text("resetPasswordToken"),
 });
+
+export const news = schema.table("news", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  title: text("title").notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("createdAt").defaultNow(),
+  announcement: text("announcement").notNull(),
+});
+
+export const faq = schema.table("faq", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  question: text("question").notNull(),
+  answer: text("answer").notNull(),
+});
+
+export const release = schema.table("release", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  preview: text("preview").notNull(),
+  title: text("title").notNull(),
+  date: timestamp("date").notNull(),
+  upc: text("upc").notNull(),
+  track: text("track").notNull(),
+});
+
+export const genre = schema.table("genre", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+});
+
+export const version = schema.table("version", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+});
+
+export const releaseType = schema.table("releaseType", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+});
