63ea95115e403f4a2bd24718d91ff5fc008691d5
TheBaconStyle <maks.christmas2001@gmail.com>
2025-07-30 20:01:32 +0200

    fix(frontend): release sending fix

--- apps/frontend/components/NewRelize/action.ts
+++ apps/frontend/components/NewRelize/action.ts
@@ -1,11 +1,10 @@
 'use server';
-import { TReleaseInsertForm, TTrackInsertForm } from 'shared/schema/release.schema';
-import { cookies } from 'next/headers';
 import { sessionCookieName } from '@/shared/lib/config/auth';
 import { createSDKConnection } from '@/shared/lib/config/sdk';
-import { functional } from 'sdk';
 import { TrueOmit } from '@/shared/lib/types';
-import { TCreateReleaseBody } from 'sdk/lib/release/release.controller';
+import { cookies } from 'next/headers';
+import { functional } from 'sdk';
+import { TReleaseInsertForm, TTrackInsertForm } from 'shared/schema/release.schema';
 
 export async function action(releaseData: TrueOmit<TReleaseInsertForm, 'preview' | 'tracks'> & { preview: string } & {
 	tracks: (TrueOmit<TTrackInsertForm, 'video' | 'video_shot' | 'ringtone' | 'text_sync' | 'track'> & { video?: string, video_shot?: string, ringtone?: string, text_sync?: string, track: string })[]
@@ -29,13 +28,19 @@ export async function action(releaseData: TrueOmit<TReleaseInsertForm, 'preview'
 
 	const NewRelize = await functional.v1.releases.createRelease(connection, {
 		release:{
-			...release, 
+			...release,
+			roles: JSON.stringify(release.roles),
+			area: JSON.stringify(release.area),
+			platforms: JSON.stringify(release.platforms),
 			releaseDate: new Date(release.releaseDate).toISOString(),
 			startDate: new Date(release.startDate).toISOString(),
 			preorderDate: new Date(release.preorderDate).toISOString(),
 			yandexSoonNewRelease: release.yandexSoonNewRelease ? new Date(release.yandexSoonNewRelease).toISOString() : undefined,
 		},
-		tracks: tracks.map(track=>({...track, instant_gratification: track.instant_gratification ? new Date(track.instant_gratification).toISOString() : undefined}))
+		tracks: tracks.map(track=>({...track,
+			instant_gratification: track.instant_gratification ? new Date(track.instant_gratification).toISOString() : undefined, 
+			roles: JSON.stringify(track.roles)
+		})),
 	});
 
 	return NewRelize;
