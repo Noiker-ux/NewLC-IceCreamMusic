13045d4537b7cd4965ec1f081875bc209e18414f
Noiker-UX <pasha_pasha_201@mail.ru>
2025-07-31 00:17:07 +0300

    sasdd

--- apps/frontend/components/Relizes/RelizesList.tsx
+++ apps/frontend/components/Relizes/RelizesList.tsx
@@ -7,7 +7,7 @@ export default async function RelizesList() {
 		<div className='flex flex-col gap-5'>
 			{arrRelizes.data.map((relize) => (
 				<div key={relize.id}>
-					<RelizecCard release={release} />
+					<RelizecCard release={arrRelizes.data} />
 				</div>
 			))}
 		</div>
